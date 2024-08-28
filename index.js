5de58441daa9cfe9f39c64fc8dcffaba391fd39b
Manish kumar <manish620287@gmail.com>
2024-08-28 19:37:39 +0530

    file setting

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', function(req, res){
-    fs.readdir(`./files`, function(err, file){
-        console.log(file);
+    fs.readdir(`./files`, function(err, files){
+        res.render('index', {files: files}); 
     })
-    res.render('index');
+})
+
+app.post('/create', function(req, res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+        res.redirect("/")
+    });
 })
 
 app.listen(3000, function(){
